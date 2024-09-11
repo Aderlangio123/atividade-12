5ca955c097dab93991ec7b7905e5d70229e0e560
Aderlangio123 <faderlangio123@gmail.com>
2024-09-11 15:30:12 -0300

    atividade 12 - verificar nota

---
+++ atividade12.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba a nota de um aluno e informe se ele foi aprovado ou reprovado. Considere que a média para aprovação é 7.
+
+nota = float(input("digite sua nota! "))
+
+if nota >= 7:
+    print("Voce esta aprovado!! ")
+else:
+    print("voce esta reprovado!! ")
+
+    
